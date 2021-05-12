b1239a08ab1b035704e8780ba3613b87e841bec4
MoD3223 <44062662+MoD3223@users.noreply.github.com>
2021-05-12 14:57:11 +0200

    Add project files.

---
+++ Vowel Count/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Vowel_Count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString = Console.ReadLine().ToLower();
+            int vowelsCount = 0;
+
+            var vowels = new List<char> { 'a', 'e', 'i', 'o', 'u' };
+
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                if (vowels.Contains(inputString[i]))
+                {
+                    vowelsCount++;
+                }
+            }
+            Console.WriteLine("The number of vowels is: " + vowelsCount);
+            Console.ReadKey();
+        }
+    }
+}
